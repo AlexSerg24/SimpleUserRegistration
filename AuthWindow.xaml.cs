86de7687a83ad05927c04e33e25429995694ba39
AlexSerg24 <ars13@tpu.ru>
2022-09-06 14:03:21 +0700

    add project

---
+++ AuthWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UserApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWindow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAuthClick(object sender, RoutedEventArgs e)
+        {
+            string login = Login.Text.Trim();
+            string password = Password.Password.Trim();
+
+            int check = 0;
+
+            if (login.Length < 5)
+            {
+                Login.ToolTip = "Слишком короткий логин!";
+                Login.Background = Brushes.DarkRed;
+            }
+            else
+            {
+                Login.ToolTip = "";
+                Login.Background = Brushes.Transparent;
+                check++;
+            }
+            
+            if (password.Length < 5)
+            {
+                Password.ToolTip = "Слишком короткий пароль!";
+                Password.Background = Brushes.DarkRed;
+            } else
+            {
+                Password.ToolTip = "";
+                Password.Background = Brushes.Transparent;
+                check++;
+            }
+
+            if (check == 2)
+            {
+                User authUser = null;
+                using (AppContext context = new AppContext())
+                {
+                    authUser = context.Users.Where(b => b.Login == login && b.Password == password).FirstOrDefault();
+                }
+
+                if (authUser != null)
+                {
+                    UserPageWindow userPageWindow = new UserPageWindow();
+                    userPageWindow.Show();
+                    Hide();
+                }
+                else
+                    MessageBox.Show("Введены некорректные данные!");
+            }
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Hide();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace UserApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AppContext db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            db = new AppContext();
+
+            DoubleAnimation btnAnim = new DoubleAnimation();
+            btnAnim.From = 100;
+            btnAnim.To = 450;
+            btnAnim.Duration = TimeSpan.FromSeconds(3);
+            regBtn.BeginAnimation(Button.WidthProperty, btnAnim);
+        }
+
+        private void BtnRegClick(object sender, RoutedEventArgs e)
+        {
+            string login = Login.Text.Trim();
+            string password = Password.Password.Trim();
+            string chaeckPassword = CheckPassword.Password.Trim();
+            string email = Email.Text.Trim();
+
+            int check = 0;
+
+            if (login.Length < 5)
+            {
+                Login.ToolTip = "Слишком короткий логин!";
+                Login.Background = Brushes.DarkRed;
+            } else
+            {
+                Login.ToolTip = "";
+                Login.Background = Brushes.Transparent;
+                check++;
+            }
+            
+            if (password.Length < 5)
+            {
+                Password.ToolTip = "Слишком короткий пароль!";
+                Password.Background = Brushes.DarkRed;
+            } else
+            {
+                Password.ToolTip = "";
+                Password.Background = Brushes.Transparent;
+                check++;
+            }
+            
+            if (password != chaeckPassword)
+            {
+                CheckPassword.ToolTip = "Пароли не совпали!";
+                CheckPassword.Background = Brushes.DarkRed;
+            } else
+            {
+                CheckPassword.ToolTip = "";
+                CheckPassword.Background = Brushes.Transparent;
+                check++;
+            }
+            
+            if (email.Length < 5 || !email.Contains("@") || !email.Contains(".")) 
+            {
+                Email.ToolTip = "Почта введена некорректно!";
+                Email.Background = Brushes.DarkRed;
+            } else
+            {
+                Email.ToolTip = "";
+                Email.Background = Brushes.Transparent;
+                check++;
+            }
+
+            if (check == 4)
+            {
+                MessageBox.Show("Регистрация прошла успешно!");
+                User newUser = new User(login, password, email);
+                db.Users.Add(newUser);
+                db.SaveChanges();
+
+                AuthWindow authWindow = new AuthWindow();
+                authWindow.Show();
+                this.Hide();
+            }
+        }
+
+        private void Button_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            AuthWindow authWindow = new AuthWindow();
+            authWindow.Show();
+            this.Hide();
+        }
+    }
+}
